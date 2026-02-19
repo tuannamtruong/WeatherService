9f8ff5d317b4df125b6a2afad9612d9d3b7e3572
Tuan Nam Truong <Truong@Funkinform.de>
2026-02-19 18:06:19 +0100

    Reading weather information of karlsruhe.

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # WeatherService
 
 Idea: https://roadmap.sh/projects/weather-api-wrapper-service
+
 API: https://www.visualcrossing.com/weather-api/
+
 Cache: Redis
+
 Go Structure: https://github.com/golang-standards/project-layout
--- cmd/weatherApi/weather.go
+++ cmd/weatherApi/weather.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Config struct {
+	ApiKey string `json:"ApiKey"`
+}
+
+// Reads and parses JSON
+// or terminates the process if failed
+func MustLoadConfig(path string) Config {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatalf("CRITICAL: Failed to read config file at %s: %v", path, err)
+	}
+	var cfg Config
+	if err = json.Unmarshal(data, &cfg); err != nil {
+		log.Fatalf("CRITICAL: Failed to parse config JSON: %v", err)
+	}
+	return cfg
+}
 
 func main() {
-	fmt.Println("HELLO.")
+	// data, err := os.ReadFile("config.json")
+	// if err != nil {
+	// 	fmt.Println("Error reading file:", err)
+	// 	return
+	// }
+
+	// Call API
+	if true {
+		config := MustLoadConfig("config.json")
+
+		// Reading weather data for city
+		weatherEndpoint := fmt.Sprintf("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Karlsruhe?unitGroup=metric&key=%s&contentType=json", config.ApiKey)
+
+		resp, err := http.Get(weatherEndpoint)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(string(body))
+	}
+
 }
